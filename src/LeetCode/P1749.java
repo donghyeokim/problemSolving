7ba357a9ab1122b26e78903355b7d1962d2498b2
donghyeok.im <donghyeok.im@nhnpayco.com>
2024-12-18 22:45:26 +0900

    리트코드-1749

---
+++ src/LeetCode/P1749.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int maxSum = 0;
+        int minSum = 0;
+        int maxSumMax = 0;
+        int minSumMin = 0;
+        for (int num : nums) {
+            maxSum += num;
+            if (maxSum < 0) maxSum = 0;
+            maxSumMax = Math.max(maxSumMax, maxSum);
+
+            minSum += num;
+            if (minSum > 0) minSum = 0;
+            minSumMin = Math.min(minSumMin, minSum);
+        }
+        return Math.max(maxSumMax, -minSumMin);
+    }
+}
+
+/*
+- num의 부분 배열에서 절대값의 합 중 최대값을 구해라
+- 길이 <= 10만
+- 카데인 알고리즘
+*/
